3920fb5ab4fb8b1db6a4c67ac43665f98a26e5f3
robmikh <rob.mikh@outlook.com>
2022-07-04 15:08:34 -0700

    cross-plat

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
 mod args;
-mod file;
 
-use std::{path::PathBuf, str::FromStr};
+use std::{path::PathBuf, str::FromStr, fs::File};
 
 use args::Args;
 use clap::StructOpt;
-use file::{create_storage_file_from_path, get_full_path_name};
-use windows::{
-    core::Result,
-    Graphics::Imaging::{BitmapDecoder, BitmapEncoder},
-    Storage::{CreationCollisionOption, FileAccessMode, StorageFile},
-    Win32::System::WinRT::{RoInitialize, RO_INIT_MULTITHREADED},
-};
+use image::EncodableLayout;
 
-fn main() -> Result<()> {
-    unsafe {
-        RoInitialize(RO_INIT_MULTITHREADED)?;
-    }
-
+fn main() {
     let args = Args::parse();
 
-    let input_full_path = get_full_path_name(&args.input)?;
-    let input_file = StorageFile::GetFileFromPathAsync(input_full_path)?.get()?;
-
+    let input_path = &args.input;
     let output_path = if let Some(output) = args.output {
         output
     } else {
@@ -30,30 +17,15 @@ fn main() -> Result<()> {
         temp_path.set_extension("gif");
         temp_path.to_str().unwrap().to_owned()
     };
-    let output_file =
-        create_storage_file_from_path(&output_path, CreationCollisionOption::ReplaceExisting)?;
 
-    let (bytes, width, height, format, alpha_mode) = {
-        let stream = input_file.OpenReadAsync()?.get()?;
-        let decoder = BitmapDecoder::CreateAsync(stream)?.get()?;
-        let frame = decoder.GetFrameAsync(0)?.get()?;
-        let width = frame.PixelWidth()?;
-        let height = frame.PixelHeight()?;
-        let format = frame.BitmapPixelFormat()?;
-        let alpha_mode = frame.BitmapAlphaMode()?;
+    let input_image = image::open(input_path).unwrap();
+    let width = input_image.width();
+    let height = input_image.height();
+    let input_rgba = input_image.as_rgba8().unwrap();
+    let mut input_bytes: Vec<u8> = input_rgba.as_bytes().iter().map(|x| *x).collect();
 
-        let pixel_data = frame.GetPixelDataAsync()?.get()?;
-        let bytes = pixel_data.DetachPixelData()?;
-
-        (bytes, width, height, format, alpha_mode)
-    };
-
-    {
-        let stream = output_file.OpenAsync(FileAccessMode::ReadWrite)?.get()?;
-        let encoder = BitmapEncoder::CreateAsync(BitmapEncoder::GifEncoderId()?, stream)?.get()?;
-        encoder.SetPixelData(format, alpha_mode, width, height, 1.0, 1.0, &bytes)?;
-        encoder.FlushAsync()?.get()?;
-    }
-
-    Ok(())
+    let mut output_file = File::create(output_path).unwrap();
+    let mut encoder = gif::Encoder::new(&mut output_file, width as u16, height as u16, &[]).unwrap();
+    let frame = gif::Frame::from_rgba(width as u16, height as u16, &mut input_bytes);
+    encoder.write_frame(&frame).unwrap();
 }
